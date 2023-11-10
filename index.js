b3ded2c87aeea4e4facb93686cff0d1ea2c97dce
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-11-10 21:38:21 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+import mongoose from "mongoose";
 const app = express();
-dotenv.config()
+dotenv.config();
+
+try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  } catch (error) {
+    handleError(error);
+  }
 
 app.listen(8800, () => {
     console.log("Connected to LasVegas leisure center");

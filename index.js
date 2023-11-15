8af57f196c226e1caa68857a08d38582c091bfad
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-11-15 17:10:06 +0000

    app update

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ dotenv.config();
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
+    console.log("it works");
   } catch (error) {
     throw(error);
   }
 }
 
+mongoose.connection.on("disconnected", () => {
+  console.log("mongo disconnected");
+})
+
+mongoose.connection.on("connected", () => {
+  console.log("mongo is connected");
+})
+
+app.get("/", (req,res) => {
+  res.send("something")
+})
+
 app.listen(8800, () => {
-  connect();
-    console.log("Connected to LasVegas leisure center");
+  connect()
+  console.log("Connected to LasVegas leisure center");
 });

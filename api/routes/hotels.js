6af0c616ec63b0a6a190523111f3f434d7e1b592
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-11-21 11:00:43 +0000

    app client update

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { createError } from "../utils/error.js";
-import { createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotelControl.js";
+import { countByCity, createHotel, deleteHotel, getAllHotels, getHotel, updateHotel } from "../controllers/hotelControl.js";
 import {verifyToken, verifyUser, verifyAdmin} from "../utils/verifyToken.js";
 
 const router = express.Router();
@@ -12,10 +12,10 @@ router.put("/:id", verifyAdmin, updateHotel)
 //DELETE
 router.delete("/:id", verifyAdmin, deleteHotel)
 //GET
-router.get("/:id", getHotel)
+router.get("/find/:id", getHotel);
 //GET ALL
 router.get("/", getAllHotels)
-router.get("/countByCity", getAllHotels)
+router.get("/countByCity", countByCity)
 router.get("/countByType", getAllHotels)
 
 

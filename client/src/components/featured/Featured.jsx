92c867c9680e8937c2d6236ad995bbecad228fad
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2023-11-27 10:45:20 +0000

    proxy fix

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -3,7 +3,7 @@ import useFetch from "../../hooks/useFetch"
 
 const Featured = () => {
   const { data, loading, error } = useFetch(
-    "/hotels/countByCity?cities=berlin,madrid,london"
+    "http://localhost:8800/api/hotels/countByCity?cities=berlin,madrid,london"
   );
 
   console.log(data)

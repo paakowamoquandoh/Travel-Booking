6dfa6198d09470e5f292a518e4b126d85d0db30a
Felix Ekow Amoquandoh <paakowamoquandoh@gmail.com>
2024-04-25 09:12:35 +0000

    admin init

--- admin/src/App.js
+++ admin/src/App.js
@@ -34,19 +34,39 @@ function App() {
               </ProtectedRoute>
             } />
             <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
+              <Route index element={
+                <ProtectedRoute>
+                  <List />
+                  </ProtectedRoute>
+              } />
+              <Route path=":userId" element={
+                <ProtectedRoute>
+                  <Single />
+                </ProtectedRoute>
+              } />
               <Route
                 path="new"
                 element={<New inputs={userInputs} title="Add New User" />}
               />
             </Route>
             <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
+              <Route index element={
+                <ProtectedRoute>
+                  <List />
+                  </ProtectedRoute>
+              } />
+              <Route path=":productId" element={
+                <ProtectedRoute>
+                  <Single />
+                </ProtectedRoute>
+              } />
               <Route
                 path="new"
-                element={<New inputs={productInputs} title="Add New Product" />}
+                element={
+                  <ProtectedRoute>
+                    <New inputs={productInputs} title="Add New Product" />
+                  </ProtectedRoute>
+                }
               />
             </Route>
           </Route>
